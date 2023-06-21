18047c625dc3d63af3099d2bf04c36f7304e8453
rubensmjr <rubensmettejunior@gmail.com>
2023-06-20 22:46:00 -0300

    Creating the consumer application

---
+++ consumer/consumer.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
+	"os"
+)
+
+//Get the enviroment varaibles passed into the container, using the "os" library
+var rabbit_host = os.Getenv("RABBIT_HOST")
+var rabbit_port = os.Getenv("RABBIT_PORT") 
+var rabbit_user = os.Getenv("RABBIT_USERNAME")
+var rabbit_password = os.Getenv("RABBIT_PASSWORD")
+
+func main(){
+	consume()
+
+}
+
+func consume(){
+
+	//Stablish rabbitmq connection
+	connection, err := amqp.Dial("amqp://" + rabbit_user + ":" +rabbit_password + "@" + rabbit_host + ":" + rabbit_port +"/")
+
+	if err != nil{
+		log.Fatalf("%s:%s","Failed to connect to rabbitMQ", err)
+	}
+
+	channel, err := connection.Channel()
+
+	if err != nil {
+		log.Fatalf("%s:%s","Failed to open a channel",err)
+	}
+
+	queue, err := channel.QueueDeclare(
+		"publisher", // name
+		true,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	fmt.Println("Channel and Queue established")
+
+	defer connection.Close()
+	defer channel.Close()
+
+	messages, err := channel.Consume(
+		queue.Name, // queue
+		"",     // consumer
+		false,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	  )
+
+	  if err != nil {
+		log.Fatalf("%s: %s", "Failed to register consumer", err)
+	}
+
+	forever := make(chan bool)
+
+	//routine that runs concurrently
+	go func() {
+		//loop to check the messages incoming in the queue
+		for d := range messages {
+			log.Printf("Received a message: %s", d.Body)
+			
+			d.Ack(false)
+		}
+	  }()
+	  
+	  fmt.Println("Running...")
+	  <-forever
+}
