c373de5dc6ffb8c3f9d34436bce57c4d3f63c4b5
rubensmjr <rubensmettejunior@gmail.com>
2023-06-22 23:44:23 -0300

    changed publishing method from http to terminal

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -1,86 +1,76 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
-	"github.com/julienschmidt/httprouter"
-	log "github.com/sirupsen/logrus"
-	"github.com/streadway/amqp"
+	"log"
 	"os"
+	"strings"
+
+	"github.com/streadway/amqp"
 )
 
-//Get the enviroment varaibles passed into the container, using the "os" library
+// Get the environment variables passed into the container using the "os" library
 var rabbit_host = os.Getenv("RABBIT_HOST")
-var rabbit_port = os.Getenv("RABBIT_PORT") 
+var rabbit_port = os.Getenv("RABBIT_PORT")
 var rabbit_user = os.Getenv("RABBIT_USERNAME")
 var rabbit_password = os.Getenv("RABBIT_PASSWORD")
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 
-	//create router instance
-	router := httprouter.New()
-
-	router.POST("/send/:message", func(w http.ResponseWriter, r *http.Request, p httprouter.Params){
-		submit(w,r,p)
-	})
-
-	fmt.Println("Running...")
-	log.Fatal(http.ListenAndServe(":80", router))
-}
-
-func submit(writer http.ResponseWriter, request *http.Request, p httprouter.Params) {
-	//Retrieve the value inside the parameter
-	message := p.ByName("message")
-	
-	fmt.Println("Received message: " + message)
-
-	//Stablish queue connection
-	conn, err := amqp.Dial("amqp://" + rabbit_user + ":" +rabbit_password + "@" + rabbit_host + ":" + rabbit_port +"/")
-
+	// Establish queue connection
+	conn, err := amqp.Dial("amqp://" + rabbit_user + ":" + rabbit_password + "@" + rabbit_host + ":" + rabbit_port + "/")
 	if err != nil {
 		log.Fatalf("%s: %s", "Failed to connect to RabbitMQ", err)
 	}
-
-	//Close connection when outer func is over
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-
 	if err != nil {
 		log.Fatalf("%s: %s", "Failed to open a channel", err)
 	}
-
-	//Close the channel when outer func is over
 	defer ch.Close()
 
-	//Declare the RabbitMQ queue
+	// Declare the RabbitMQ queue
 	queue, err := ch.QueueDeclare(
 		"publisher", // name
-		true,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		true,        // durable
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
 	)
-
 	if err != nil {
 		log.Fatalf("%s: %s", "Failed to declare a queue", err)
 	}
 
-	//Publish the queue in the channel
-	err = ch.Publish(
-		"",     // exchange
-		queue.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing {
-			ContentType: "text/plain",
-			Body:        []byte(message),
-	})
+	for {
+		fmt.Print("Enter a message to send: ")
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Error reading input:", err)
+			continue
+		}
 
-	if err != nil {
-		log.Fatalf("%s: %s", "Failed to publish a message", err)
+		// Trim any leading/trailing whitespaces and newlines from the message
+		message = strings.TrimSpace(message)
+
+		// Publish the message to the queue
+		err = ch.Publish(
+			"",          // exchange
+			queue.Name,  // routing key
+			false,       // mandatory
+			false,       // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(message),
+			},
+		)
+		if err != nil {
+			log.Fatalf("%s: %s", "Failed to publish a message", err)
+		}
+
+		fmt.Println("Publish success!")
 	}
-
-	fmt.Println("publish success!")
 }
